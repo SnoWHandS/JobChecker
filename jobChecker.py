8644c64a56a989c47b5ae29dbed467b7a142e035
SnoWHandS <dillon.heald@gmail.com>
2019-11-16 09:01:36 +0200

    Script fully functional, redacted private information

---
+++ jobChecker.py
@@ -0,0 +1,87 @@
+#Job checker checks if any new jobs are available at https://nursery.aerobotics.com/annotation-products
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+POLL_RATE = 300
+browser = webdriver.Firefox()
+notLogin = True
+
+while(1):
+    
+    body = browser.find_element_by_tag_name("body")
+    body.send_keys(Keys.CONTROL + 't')
+   
+    #get the page
+    browser.get('REDACTED')
+    if notLogin:
+        #Login
+        email = browser.find_element_by_id("mat-input-0")
+        email.send_keys("REDACTED")
+
+        passwrd = browser.find_element_by_id("mat-input-1")
+        passwrd.send_keys("REDACTED")
+
+        submit = browser.find_element_by_class_name("mat-button-wrapper")
+        submit.click()
+        notLogin = False
+
+
+    #check for fruit jobs
+    time.sleep(5)
+    #check High Priority Job for fruit finding
+    highFruit = browser.find_element_by_xpath('/html/body/nur-root/nur-annotation-products/div/div/div[1]/div[3]/label')
+    print "high fruit: "+highFruit.text
+    #check Medium Priority Job for fruit finding
+    medFruit = browser.find_element_by_xpath('/html/body/nur-root/nur-annotation-products/div/div/div[1]/div[2]/label')
+    print "med fruit: "+medFruit.text
+    #check High Priority Job for fruit finding
+    lowFruit = browser.find_element_by_xpath('/html/body/nur-root/nur-annotation-products/div/div/div[1]/div[1]/label')
+    print "low fruit: "+lowFruit.text
+
+    totalFruit = int(highFruit.text) + int(medFruit.text) + int(lowFruit.text)
+
+
+    #check for tree jobs
+    #check High Priority Job for fruit finding
+    highTree = browser.find_element_by_xpath('/html/body/nur-root/nur-annotation-products/div/div/div[2]/div[3]/label')
+    print "high tree: "+highTree.text
+    #check Medium Priority Job for fruit finding
+    medTree = browser.find_element_by_xpath('/html/body/nur-root/nur-annotation-products/div/div/div[2]/div[2]/label')
+    print "med tree: "+medTree.text
+    #check High Priority Job for fruit finding
+    lowTree = browser.find_element_by_xpath('/html/body/nur-root/nur-annotation-products/div/div/div[2]/div[1]/label')
+    print "low tree: "+lowTree.text
+
+    totalTree = int(highTree.text) + int(medTree.text) + int(lowTree.text)
+
+    print "total fruit: "+str(totalFruit)
+    print "total tree: "+str(totalTree)
+
+    #total all the jobs
+    total = totalFruit+totalTree
+    if total>0:
+        print str(total)+" jobs available"
+        #ring bell 3 times
+        i = 0
+        while i != 3:
+            print "\a"
+            time.sleep(0.2)
+            i = i+1
+
+    #close tab
+    body = browser.find_element_by_tag_name("body")
+    body.send_keys(Keys.CONTROL + 'w')
+    
+    #print a time stamp
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print current_time
+
+    #make a new line
+    print ""
+    #wait for polling rate
+    time.sleep(POLL_RATE)
+
